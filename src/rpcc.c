99545b82ee7bb118c595b97d294a6475b752ca68
Simon Long <simon@raspberrypi.com>
2024-12-02 14:13:56 +0000

    Skeleton

---
+++ src/rpcc.c
@@ -0,0 +1,82 @@
+/*============================================================================
+Copyright (c) 2024 Raspberry Pi Holdings Ltd.
+Some code based on lxinput from the LXDE project :
+Copyright (c) 2009-2014 PCMan, martyj19, Julien Lavergne, Andri Grytsenko
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the copyright holder nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+============================================================================*/
+
+#include <locale.h>
+#include <gtk/gtk.h>
+#include <glib/gi18n.h>
+#include "rpcc.h"
+
+/*----------------------------------------------------------------------------*/
+/* Typedefs and macros */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Global data */
+/*----------------------------------------------------------------------------*/
+
+GtkWidget *dlg;
+
+/*----------------------------------------------------------------------------*/
+/* Function prototypes */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/* Main function */
+/*----------------------------------------------------------------------------*/
+
+int main (int argc, char* argv[])
+{
+    GtkBuilder *builder;
+
+    setlocale (LC_ALL, "");
+    bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
+
+    gtk_init (&argc, &argv);
+
+    /* create the dialog */
+    builder = gtk_builder_new_from_file (PACKAGE_DATA_DIR "/ui/rpcc.ui");
+
+    dlg = (GtkWidget *) gtk_builder_get_object (builder, "dlg");
+
+    g_object_unref (builder);
+
+    /* run the dialog */
+    if (gtk_dialog_run (GTK_DIALOG (dlg)) != GTK_RESPONSE_OK)
+    {
+    }
+
+    gtk_widget_destroy (dlg);
+
+    return 0;
+}
+
+/* End of file */
+/*============================================================================*/
